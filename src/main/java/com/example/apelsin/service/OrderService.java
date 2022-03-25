cbcd5cbfa1fad3c69c5b5c561dc5212d48650905
MalikovAzizjon4843 <malikovaziz20034843@gmail.com>
2022-03-25 15:45:12 +0500

    apelsin added

--- src/main/java/com/example/apelsin/service/OrderService.java
+++ src/main/java/com/example/apelsin/service/OrderService.java
@@ -13,28 +13,6 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-    final OrderRepository orderRepository;
-    final CustomerRepository customerRepository;
-    public ApiResponse add(OrderDto dto) {
-        Order order = new Order();
-        Optional<Customer> optionalCustomer = customerRepository.findById(dto.getCustId());
-        if (optionalCustomer.isEmpty()) return new ApiResponse("Not found!", false);
-        order.setCustId(optionalCustomer.get());
-        order.setDate(dto.getDate());
-        return new ApiResponse("Added", true);
-    }
 
-    public ApiResponse edit(Integer id, OrderDto dto) {
-
-        Optional<Order> byId = orderRepository.findById(id);
-        if (byId.isEmpty()) return new ApiResponse(" Not found!", false);
-        Order order = byId.get();
-
-        order.setDate(dto.getDate());
-        Optional<Customer> optionalGM = customerRepository.findById(dto.getCustId());
-        order.setCustId(optionalGM.get());
-        orderRepository.save(order);
-        return new ApiResponse("Edited", true);
-    }
 
 }
